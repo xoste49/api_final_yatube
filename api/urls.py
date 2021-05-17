86eb4dfde017ee0f36e588234473ea527352ddd1
Pavel <xoste49@gmail.com>
2021-05-17 16:39:55 +0300

    Add route in urlpatterns

--- api/urls.py
+++ api/urls.py
@@ -1,25 +1,25 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import (
-        TokenObtainPairView,
-        TokenRefreshView,
-    )
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 from api.views import PostViewSet
 
 router = DefaultRouter()
 router.register('posts', PostViewSet, basename='posts')
 
-
 urlpatterns = [
-        path(
-            'token/',
-            TokenObtainPairView.as_view(),
-            name='token_obtain_pair'
-        ),
-        path(
-            'token/refresh/',
-            TokenRefreshView.as_view(),
-            name='token_refresh'
-        ),
-    ]
+    path('', include(router.urls)),
+    path(
+        'token/',
+        TokenObtainPairView.as_view(),
+        name='token_obtain_pair'
+    ),
+    path(
+        'token/refresh/',
+        TokenRefreshView.as_view(),
+        name='token_refresh'
+    ),
+]
