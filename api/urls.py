fdff7947cbc2aff2d40cbec64e1c7c44f076721a
Pavel <xoste49@gmail.com>
2021-05-17 15:26:29 +0300

    Add simplejwt

--- api/urls.py
+++ api/urls.py
@@ -1,4 +1,18 @@
 from django.urls import path
+from rest_framework_simplejwt.views import (
+        TokenObtainPairView,
+        TokenRefreshView,
+    )
 
 urlpatterns = [
-]
+        path(
+            'api/v1/token/',
+            TokenObtainPairView.as_view(),
+            name='token_obtain_pair'
+        ),
+        path(
+            'api/v1/token/refresh/',
+            TokenRefreshView.as_view(),
+            name='token_refresh'
+        ),
+    ]
