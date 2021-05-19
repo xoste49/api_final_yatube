444d7bccfdc422fd8cc8d3b8dab5e98ddcbde77d
Pavel <xoste49@gmail.com>
2021-05-19 16:17:58 +0300

    Add filters in FollowSerializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,9 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
-from .models import Comment, Post, Group, Follow
+from .models import Comment, Follow, Group, Post
+
+User = get_user_model()
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -27,7 +30,34 @@ class GroupSerializer(serializers.ModelSerializer):
 
 
 class FollowSerializer(serializers.ModelSerializer):
+    user = serializers.SlugRelatedField(
+        many=False,
+        read_only=True,
+        slug_field='username'
+    )
+    following = serializers.SlugRelatedField(
+        slug_field='username',
+        queryset=User.objects.all()
+    )
+
+    def validate(self, data):
+        """ Проверяем повторную подписку """
+        if Follow.objects.filter(
+                user=self.context.get('request').user,
+                following=data['following']
+        ):
+            raise serializers.ValidationError("Вы уже подписанны")
+        return data
+
+    def validate_following(self, following):
+        """ Проверяем подписку на самого себя """
+        if self.context.get('request').method == 'POST':
+            if self.context.get('request').user == following:
+                raise serializers.ValidationError(
+                    "Попытка подписаться на самого себя"
+                )
+        return following
+
     class Meta:
         fields = ('user', 'following',)
         model = Follow
-
